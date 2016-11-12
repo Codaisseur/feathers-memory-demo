b19156bf7e117b82b9fed463a958c0b055f85daf
Wouter de Vos <wouter@codaisseur.com>
2016-11-12 12:29:40 +0100

    Production webpack config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -30,17 +30,27 @@ module.exports = {
         warnings: false
       }
     }),
+    new webpack.ProvidePlugin({
+      'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+    }),
     new webpack.NoErrorsPlugin(),
     HTMLWebpackPluginConfig
   ],
   module: {
-    loaders: [{
-      test: /\.jsx?/,
-      loader: 'babel',
-      query: {
-        presets: ['es2015', 'react']
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: path.join(__dirname, 'client'),
+        exclude: [/(node_modules|bower_components)/, /\.test\.jsx?$/],
+        loader: 'babel',
+        query: {
+          presets: ['airbnb', 'react', 'es2015', 'stage-0']
+        },
       },
-      exclude: /node_modules/
-    }]
+      { test: /\.woff2?$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
+      { test: /\.(ttf|eot|svg|png|gif)$/, loader: 'file-loader' },
+      { test: /\.(sass|scss)$/, loader: 'style!css!sass'},
+      { test: /\.json$/, loader: 'json-loader'}
+    ]
   }
 };
